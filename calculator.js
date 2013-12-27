22e59f692617bb2a9bd255ed85e3e51ec65b6e43
Longkai Cao <lkcao@thoughtworks.com>
2013-12-28 00:13:17 +0800

    add the rest tests

--- calculator.js
+++ calculator.js
@@ -1,3 +1,15 @@
 add = function(num1, num2) {
   return num1 + num2;
 }
+
+substract = function(num1, num2) {
+  return num1 - num2;
+}
+
+multiply = function(num1, num2) {
+  return num1 * num2;
+}
+
+divide = function(num1, num2) {
+  return num1 / num2;
+}
--- calculatorTest.js
+++ calculatorTest.js
@@ -6,4 +6,22 @@ describe("calculator", function(){
     expect(result).toBe(expected);
   });
 
+  it("should be able to substract", function(){
+    var result = substract(1, 2);
+    var expected = -1;
+    expect(result).toBe(expected);
+  });
+
+  it("should be able to multiply", function(){
+    var result = multiply(1, 2);
+    var expected = 2;
+    expect(result).toBe(expected);
+  });
+
+  it("should be able to divide", function(){
+    var result = divide(1, 2);
+    var expected = 0.5;
+    expect(result).toBe(expected);
+  });
+
 })
